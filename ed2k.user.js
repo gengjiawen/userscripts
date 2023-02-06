73134092f40111fd83600ad99495fbecc2221bbb
Jiawen Geng <technicalcute@gmail.com>
2023-02-06 09:21:26 +0000

    add linker getter

---
+++ ed2k.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         get ed2k or other staff
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  print ed2ks to console
+// @author       You
+// @match        **/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  Array.from(document.body.getElementsByTagName("a"))
+    .filter((i) => i.getAttribute("href").startsWith("ed2k"))
+    .map((i) => i.getAttribute("href"))
+    .map((i) => console.log(i));
+})();
